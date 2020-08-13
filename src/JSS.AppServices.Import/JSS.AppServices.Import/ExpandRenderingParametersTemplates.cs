0857ba8cd9c0defe457953d24de1a30d1af7343f
Chris Gentile <chris_gentile@comcast.com>
2020-08-13 16:57:48 -0400

    Update inherited template to standard rendering parameters template.

--- src/JSS.AppServices.Import/JSS.AppServices.Import/ExpandRenderingParametersTemplates.cs
+++ src/JSS.AppServices.Import/JSS.AppServices.Import/ExpandRenderingParametersTemplates.cs
@@ -4,11 +4,13 @@
 
     public class ExpandRenderingParametersTemplates : Sitecore.JavaScriptServices.AppServices.Pipelines.Import.ExpandRenderingParametersTemplates
     {
+        private readonly string StandardRenderingParametersTemplate = "{8CA06D6A-B353-44E8-BC31-B528C7306971}";
+
         protected override TemplateDef CreateTemplateDefFromRenderingParametersDefs(RenderingDef rendering)
         {
             var result = base.CreateTemplateDefFromRenderingParametersDefs(rendering);
 
-            result.Inherits = new[] { Sitecore.TemplateIDs.StandardTemplate.ToString() };
+            result.Inherits = new[] { StandardRenderingParametersTemplate };
 
             return result;
         }
